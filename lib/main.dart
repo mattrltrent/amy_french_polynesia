312a4b7e137fa00c18cd202c3b338b8d43f3a9fc
Matthew <matthew.rl.trent@gmail.com>
2023-02-28 20:00:00 -0600

    fixed safearea issue
    Co-authored-by: amy <amy@trentfamily.com>

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BottomNav(),
+      home: const SafeArea(child: BottomNav()),
     );
   }
 }
